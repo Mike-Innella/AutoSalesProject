5fcb206564e4123fc408e2e43c6c3e3c654e5da6
Michael Innella <mainnella@gmail.com>
2024-12-19 18:14:00 -0500

    modal and backdrop
    finished modal backdrop and open close modal functions

--- apps.js
+++ apps.js
@@ -1,22 +1,47 @@
+//MODAL
 const openModal = document.getElementById("openModal");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const modal = document.getElementById("contactModal");
 const backdrop = document.querySelector(".modal__backdrop");
+const landingPage = document.querySelector(".landing__page");
+const navBar = document.querySelector(".nav__container");
 
 // Open Modal
 openModal.addEventListener("click", () => {
   modal.classList.add("active");
   backdrop.classList.add("active");
+  backdrop.style.display = "block";
+  backdrop.style.visibility = "visible";
+  setTimeout(() => {
+    backdrop.style.opacity = "1";
+    navBar.style.opacity = "0";
+    landingPage.style.opacity = "0";
+  }, 200);
 });
 
 // Close Modal
 closeModalBtn.addEventListener("click", () => {
+  backdrop.style.visibility = "hidden";
   modal.classList.remove("active");
   backdrop.classList.remove("active");
+  setTimeout(() => {
+    backdrop.style.opacity = "0";
+  }, 200);
+  setTimeout(() => {
+    landingPage.style.opacity = "1";
+    navBar.style.opacity = "1";
+  }, 400);
 });
 
-// Close Modal when clicking backdrop
 backdrop.addEventListener("click", () => {
+  backdrop.style.visibility = "hidden";
   modal.classList.remove("active");
   backdrop.classList.remove("active");
+  setTimeout(() => {
+    backdrop.style.opacity = "0";
+  }, 200);
+  setTimeout(() => {
+    landingPage.style.opacity = "1";
+    navBar.style.opacity = "1";
+  }, 400);
 });

079d025e282287aecb08f424d69a93072df18b37
Michael Innella <mainnella@gmail.com>
2024-12-19 10:15:43 -0500

    modal development
    modified styling to search bar and began development of modal, semifunctional at this time.

---
+++ apps.js
@@ -0,0 +1,16 @@
+const openModal = document.getElementById("openModal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("contactModal");
+const backdrop = document.querySelector(".modal__backdrop");
+
+// Open Modal
+openModal.addEventListener("click", () => {
+  modal.classList.add("active");
+  backdrop.classList.add("active");
+});
+
+// Close Modal
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+  backdrop.classList.remove("active");
+});
